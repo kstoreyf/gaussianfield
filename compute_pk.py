attempt to fix scaling and pk

compute_pk.py
@@ -8,55 +8,73 @@ import gaussian_random as gr
 
 def main():
     L = 40
-    n = 256
+    n = 64
     d = 3
 
-    # Plot true power spectrum
-    klog = np.logspace(np.log10(1e-3), np.log10(1e2), 50)
-    pktrue = gr.powspec(klog)
-    plot_pk(klog, pktrue)
+    # Calculate true power spectrum
+    ktrue = np.logspace(np.log10(1e-3), np.log10(1e2), 50)
+    pktrue = gr.powspec(ktrue)
 
-    # Plot true correlation function
-    rtrue, cftrue = pk_to_2pcf(klog, pktrue)
-    plot_cf(rtrue, cftrue)
+    # Calculate true correlation function
+    rtrue, cftrue = pk_to_2pcf(ktrue, pktrue, d)
 
     # Calculate output power spectrum from density field
+
     dens = gr.load_field('density_n{}_L{}_{}d.p'.format(n, L, d))
-    pk = np.abs(fftengn.fftshift(fftengn.rfftn(dens)))**2
-    pk *= float(n)**(2*d)/(L**d) #rescale
-    k = gr.get_ks(L, n, d)
+    dens /= 1./(2*np.pi)**d
+    # fft to properly count pixels
+    pk = np.abs(fftengn.fftshift(fftengn.fftn(dens)))**2
+
+    #scale
+    boxvol = np.float(L)**3
+    pixelsize = boxvol/np.float(n)**3
+    unscale = boxvol/pixelsize**2
+    pk *= unscale
+
+    # get all frequency values
+    k = gr.get_ks(L, n, d, full=True)
+
     kavg, pkavg = radial_average(k, pk)
-    plot_pk(kavg, pkavg)
+    plot_pk(*zip([ktrue, pktrue], [kavg, pkavg]))
+
 
     # Calculate correlation function from output power spectrum
-    r, cf = pk_to_2pcf(kavg, pkavg)
-    plot_cf(r, cf)
+    r, cf = pk_to_2pcf(kavg, pkavg, d)
+    plot_cf(*zip([rtrue, cftrue], [r, cf]))
 
     plt.show()
 
 
 def plot_cf(r, cf):
     plt.figure()
-    rcf = np.array([rrcf for rrcf in zip(r, cf) if rrcf[1]>0])
-    rr = rcf[:,0]
-    cfcf = rcf[:,1]
-    plt.loglog(rr, cfcf)
+    rsize = np.array(r).shape[0]
+    if rsize==1:
+        r = [r]
+        cf = [cf]
+    for i in range(rsize):
+        rcf = np.array([rrcf for rrcf in zip(r[i], cf[i]) if rrcf[1]>0])
+        rr = rcf[:,0]
+        cfcf = rcf[:,1]
+        plt.loglog(rr, cfcf)
     plt.xlabel('r')
     plt.ylabel(r'$\xi$(r)')
 
 
-def pk_to_2pcf(k, pk):
+def pk_to_2pcf(k, pk, d):
     print 'Pk tp 2PCF'
     ksps = np.array([kkpp for kkpp in zip(k, pk) if np.isfinite(kkpp[1])])
     ks = ksps[:,0]
     ps = ksps[:,1]
-    cf = 1./(2*np.pi)**3 * fftengn.fftshift(fftengn.irfft(ps))
+    cf = 1./(2*np.pi)**d * fftengn.fftshift(fftengn.irfft(ps))
     r = 2.*np.pi/ks
     return r, cf
 
 
 def radial_average(x, y):
     print 'Averaging power'
+
+    #x, y = zero_odds(x,y)
+
     xflat = x.flatten()
     yflat = y.flatten()
     xmin = np.min(xflat[np.nonzero(xflat)])
@@ -76,9 +94,29 @@ def radial_average(x, y):
     return xavg, yavg
 
 
+def zero_odds(x, y):
+    n = x.shape[0]
+    kx = fftengn.fftshift(fftengn.fftfreq(n, 1./n))
+    ky = fftengn.fftshift(fftengn.fftfreq(n, 1./n))
+    kz = fftengn.fftshift(fftengn.fftfreq(n, 1./n))[:n/2+1]
+    #mask = np.zeros((n,n,n))
+    for i in range(len(kx)):
+        for j in range(len(ky)):
+            for k in range(len(kz)):
+                if kx[i]%2!=0 or ky[j]%2!=0 or kz[k]%2!=0:
+                    x[i][j][k] = 0
+                    y[i][j][k] = 0
+    return x, y
+
+
 def plot_pk(k, Pk):
     plt.figure()
-    plt.loglog(k, Pk)
+    ksize = np.array(k).shape[0]
+    if ksize==1:
+        k = [k]
+        Pk = [Pk]
+    for i in range(ksize):
+        plt.loglog(k[i], Pk[i])
     plt.xlabel('k')
     plt.ylabel('P(k)')
 

gaussian_random.py
@@ -5,19 +5,19 @@ from numpy import fft as fftengn
 from scipy.optimize import curve_fit
 
 
-pickle_dir = 'pickles_2018-10-16/'
-plot_dir = 'plots_2018-10-16/'
+pickle_dir = 'pickles_2018-10-28/'
+plot_dir = 'plots_2018-10-28/'
 
 
 def main():
 
     L = 40 #length scale
-    n = 256 #pixels on side of density field
+    n = 64 #pixels on side of density field
     d = 3 #dimensions
     N = int(1e3) #num samples
 
     generate_field(L, n, d)
-    sample_field(L, n, N, d)
+    #sample_field(L, n, N, d)
     plt.show()
 
 
@@ -29,32 +29,32 @@ def generate_field(L, n, d):
 
     dens = density_field(a, d)
     plot_pixel_hist(dens, fit=True)
-    plot_density(dens, d, avg=False, saveto='density'+tag+'.png')
-    plot_density(dens, d, avg=True, saveto='density_avg'+tag+'.png')
-    plot_tiled(dens, d, avg=True, saveto='density_tiled'+tag+'.png')
+    plot_density(dens, L, n, d, avg=False, saveto='density'+tag+'.png')
+    plot_density(dens, L, n, d, avg=True, saveto='density_avg'+tag+'.png')
+    plot_tiled(dens, L, n, d, avg=True, saveto='density_tiled'+tag+'.png')
     save_field(dens, 'density'+tag+'.p')
 
 
 def sample_field(L, n, N, d):
     tag = '_n{}_L{}_{}d'.format(n, L, d)
     dens = load_field('density'+tag+'.p')
-    q = exponentiate(dens, N, fac=1e9)
+    q = exponentiate(dens, N, fac=5e7)
     samples = generate_samples(L, N, d)
     sampled_field, rejects = reject_samples(samples, q, L, n, d)
     plot_samples(sampled_field, rejects, d)
     save_field(sampled_field, 'samples'+tag+'_N{}.p'.format(N))
 
 
-def get_ks(L, n, d):
+def get_ks(L, n, d, full=False):
     print 'Computing ks'
     d = int(d)
     assert d in [1,2,3], 'd must be 1, 2, or 3'
     if d==1:
-        return get_ks1d(L, n)
+        return get_ks1d(L, n, full=full)
     elif d==2:
-        return get_ks2d(L, n)
+        return get_ks2d(L, n, full=full)
     else:
-        return get_ks3d(L, n)
+        return get_ks3d(L, n, full=full)
 
 
 def get_amplitudes(L, n, Pk, d):
@@ -68,19 +68,28 @@ def get_amplitudes(L, n, Pk, d):
     else:
         return get_amplitudes3d(L, n, Pk)
 
+def density_field(a, d):
+    print 'Transforming amplitudes to density field'
+    print a
+    dens = 1./(2*np.pi)**d * fftengn.ifftn(a)
+    assert abs(np.max(np.imag(dens)))<1e-15, 'Density field should be entirely real'
+    return np.real(dens)
 
-def get_ks3d(L, n):
+
+def get_ks3d(L, n, full):
     assert n % 2 == 0
     kx = fftengn.fftshift(fftengn.fftfreq(n, 1./n))
     ky = fftengn.fftshift(fftengn.fftfreq(n, 1./n))
-    kz = fftengn.fftshift(fftengn.fftfreq(n, 1./n))[:n/2+1]
+    kz = fftengn.fftshift(fftengn.fftfreq(n, 1./n))
+    if not full:
+        kz = kz[:n/2+1]
     k = 2.*np.pi/L * np.sqrt(kx[:,np.newaxis][np.newaxis,:]**2
                            + ky[:,np.newaxis][:,np.newaxis]**2
                            + kz[np.newaxis,:][np.newaxis,:]**2)
     return k
 
 
-def get_amplitudes3d(L, n, Pk):
+def get_amplitudes3d_oldscaling(L, n, Pk):
     deltan = (float(L)/float(n))**-3
     areal = np.zeros((n,n,n))
     aim = np.zeros((n,n,n))
@@ -101,16 +110,44 @@ def get_amplitudes3d(L, n, Pk):
     return a
 
 
-def get_ks2d(L, n):
+def get_amplitudes3d(L, n, Pk):
+    #deltan = (float(L)/float(n))**-3
+    areal = np.zeros((n,n,n))
+    aim = np.zeros((n,n,n))
+    for i in range(n):
+        for j in range(n):
+            for g in range(n/2+1):
+                pk = Pk[i][j][g]
+                if (i==0 or i==n/2) and (j==0 or j==n/2) and (g==0 or g==n/2):
+                    areal[i][j][g] = 2**(-0.5) * np.random.normal(0, np.sqrt(pk))
+                    aim[i][j][g] = 0
+                else:
+                    areal[i][j][g] = 2**(-0.5) * np.random.normal(0, np.sqrt(pk))
+                    aim[i][j][g] = 2**(-0.5) * np.random.normal(0, np.sqrt(pk))
+                    areal[(n-i)%n][(n-j)%n][(n-g)%n] = areal[i][j][g]
+                    aim[(n-i)%n][(n-j)%n][(n-g)%n] = -aim[i][j][g]
+    a = areal + 1.0j*aim
+    #scale
+    boxvol = float(L) ** 3
+    pixelsize = boxvol / float(n) ** 3
+    scale = pixelsize ** 2 / boxvol
+    a *= np.sqrt(scale)
+    a = fftengn.ifftshift(a)
+    return a
+
+
+def get_ks2d(L, n, full=False):
     assert n % 2 == 0
     kx = fftengn.fftshift(fftengn.fftfreq(n, 1./n))
-    ky = fftengn.fftshift(fftengn.fftfreq(n, 1./n))[:n/2+1]
+    ky = fftengn.fftshift(fftengn.fftfreq(n, 1./n))
+    if not full:
+        ky = ky[:n/2+1]
     k = 2.*np.pi/L * np.sqrt(kx[:,np.newaxis]**2
                            + ky[np.newaxis,:]**2)
     return k
 
 
-def get_amplitudes2d(L, n, Pk):
+def get_amplitudes2d_oldscaling(L, n, Pk):
     deltan = (float(L)/float(n))**-2
     areal = np.zeros((n,n))
     aim = np.zeros((n,n))
@@ -130,14 +167,63 @@ def get_amplitudes2d(L, n, Pk):
     return a
 
 
-def get_ks1d(L, n):
+def get_amplitudes2d(L, n, Pk):
+    areal = np.zeros((n,n))
+    aim = np.zeros((n,n))
+    for i in range(n):
+        for j in range(n/2+1):
+            pk = Pk[i][j]
+            if (i==0 or i==n/2) and (j==0 or j==n/2):
+                areal[i][j] = np.random.normal(0, np.sqrt(pk))
+                aim[i][j] = 0
+            else:
+                areal[i][j] = np.random.normal(0, np.sqrt(pk))
+                aim[i][j] = np.random.normal(0, np.sqrt(pk))
+                areal[(n-i)%n][(n-j)%n] = areal[i][j]
+                aim[(n-i)%n][(n-j)%n] = -aim[i][j]
+    a = areal + 1.0j*aim
+    #scale
+    boxvol = float(L) ** 2
+    pixelsize = boxvol / float(n) ** 2
+    scale = pixelsize ** 2 / boxvol
+    a *= np.sqrt(scale)
+    a = fftengn.ifftshift(a)
+    return a
+
+
+def get_ks1d(L, n, full=False):
     assert n % 2 == 0
-    kx = fftengn.fftshift(fftengn.fftfreq(n, 1./n))[:n/2+1]
+    kx = fftengn.fftshift(fftengn.fftfreq(n, 1./n))
+    if not full:
+        kx = kx[:n/2+1]
     k = 2.*np.pi/L * abs(kx)
     return k
 
 
 def get_amplitudes1d(L, n, Pk):
+    areal = np.zeros(n)
+    aim = np.zeros(n)
+    for i in range(n/2+1):
+        pk = Pk[i]
+        if (i==0 or i==n/2):
+            areal[i] = 2**(-0.5) * np.random.normal(0, np.sqrt(pk))
+            aim[i] = 0
+        else:
+            areal[i] = 2**(-0.5) * np.random.normal(0, np.sqrt(pk))
+            aim[i] = 2**(-0.5) * np.random.normal(0, np.sqrt(pk))
+            areal[(n-i)%n] = areal[i]
+            aim[(n-i)%n] = -aim[i]
+    a = areal + 1.0j*aim
+    #scale
+    boxvol = float(L)
+    pixelsize = boxvol / float(n)
+    scale = pixelsize ** 2 / boxvol
+    a *= np.sqrt(scale)
+    a = fftengn.ifftshift(a)
+    return a
+
+
+def get_amplitudes1d_oldscaling(L, n, Pk):
     deltan = (float(L)/float(n))**-1
     areal = np.zeros(n)
     aim = np.zeros(n)
@@ -154,14 +240,6 @@ def get_amplitudes1d(L, n, Pk):
     a = areal + 1.0j*aim
     a = fftengn.ifftshift(a)
     return a
-
-
-def density_field(a, d):
-    print 'Transforming amplitudes to density field'
-    dens = 1./(2*np.pi)**d * fftengn.ifftn(a)
-    assert abs(np.max(np.imag(dens)))<1e-15, 'Density field should be entirely real'
-    return np.real(dens)
-
 
 def exponentiate(dens, N, fac):
     print 'Exponentiating density field'
@@ -180,7 +258,7 @@ def load_field(fn):
     return np.load(pickle_dir+fn)
 
 
-def plot_density(dens, d, avg=True, saveto=None):
+def plot_density(dens, n, L, d, avg=True, saveto=None):
     plt.figure()
     if d==1:
         plt.plot(dens)
@@ -204,7 +282,7 @@ def plot_density(dens, d, avg=True, saveto=None):
         plt.savefig(plot_dir+saveto)
 
 
-def plot_tiled(dens, d, avg=True, saveto=None):
+def plot_tiled(dens, L, n, d, avg=True, saveto=None):
     plt.figure()
     if d==1:
         plt.plot(np.tile(dens, 2))
@@ -233,12 +311,13 @@ def reject_samples(samples, q, L, n, d):
     rejects = []
     count = 0
     for s in samples:
-        count +=1
-        px = int(np.floor(s[0]/n))
+        count += 1
+        px = int(np.floor(s[0]*n/float(L)))
         if d>=2:
-            py = int(np.floor(s[1]/n))
+            py = int(np.floor(s[1]*n/float(L)))
+            print px, py
         if d==3:
-            pz = int(np.floor(s[2]/n))
+            pz = int(np.floor(s[2]*n/float(L)))
 
         if d==1:
             qval = q[px]